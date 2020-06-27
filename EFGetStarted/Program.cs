c9048e13ea183eeae5dc36e50715aea485b99f2f
Ruslan391 <aleks.afanasev@list.ru>
2020-06-27 19:57:35 +0300

    Выборка1

--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
  
 namespace EFGetStarted
 
@@ -12,16 +13,23 @@ namespace EFGetStarted
             {
                 Student student1 = new Student {Name = "Ruslan", Surname = "Meboniya", Age = 28, Gender = "Male"};
                 Student student2 = new Student {Name = "Roma", Surname = "Shulga", Age = 28, Gender = "Male"};
+                Student student3 = new Student {Name = "Dmitrii", Surname = "Juravlev", Age = 28, Gender = "Male"};
+                Student student4 = new Student {Name = "Sergei", Surname = "Suslov", Age = 28, Gender = "Male"};
                 //Person person3 = new Person {Name = "Sergei", Surname = "Shekshuev", Age = "28", Gender = "Female"};
-                db.Persons.AddRange(student1,student2);
+                //db.Persons.AddRange(student3,student4,student3,student4);
+                db.Persons.Add(student1);
+                db.Persons.Add(student2);
+                db.Persons.Add(student3);
+                db.Persons.Add(student4);
                 db.SaveChanges();
                 Console.WriteLine("Пёрсоны добавлены");
 //    Вывод пёрсонов
-                var users1 = db.Persons.ToList();
-                Console.WriteLine("Список Пёрсонов:");
-                foreach (Person q in users1)
+               
+                var stud= (from s in db.Persons where s.Discriminator=="Student" select s).ToList();
+                Console.WriteLine("Список Students:");
+                foreach (Person qq in stud)
                 {
-                    Console.WriteLine($"{q.Id}.Имя - {q.Name}, Возраст - {q.Age}, пол - {q.Gender}, роль - {q.Discriminator}");
+                    Console.WriteLine($"{qq.Id}.Имя - {qq.Name}, Возраст - {qq.Age}, пол - {qq.Gender}, роль - {qq.Discriminator}");
                 }
 
 
@@ -30,16 +38,23 @@ namespace EFGetStarted
                 db.Persons.AddRange(teacher1,teacher2);
                 db.SaveChanges();
                 Console.WriteLine("Тичеры добавлены");
+                 var teach= (from t in db.Persons where t.Discriminator=="Teacher" select t).ToList();
+
+                Console.WriteLine("Список Teachers:");
+                foreach (Person q in teach)
+                {
+                    Console.WriteLine($"{q.Id}.Имя - {q.Name}, Возраст - {q.Age}, пол - {q.Gender}, роль - {q.Discriminator}");
+                }
 //    Вывод до изменения
-                Console.WriteLine("Список объектов до того как Шекша изнасиловали:");
-                var users2 = db.Persons.ToList();
-                foreach (Person q in users2)
+                Console.WriteLine("Список Teachers до того как Шекша изнасиловали:");
+                //var users2 = db.Persons.ToList();
+                foreach (Person q in teach)
                 {
                     Console.WriteLine($"{q.Id}.Имя - {q.Name}, Возраст - {q.Age}, пол - {q.Gender}, роль - {q.Discriminator}");
                 }
 //     Изменение
-                Console.WriteLine("Список объектов после того как Шекша изнасиловали:");
-                foreach (Person q in users2)
+                Console.WriteLine("Список Teachers после того как Шекша изнасиловали:");
+                foreach (Person q in teach)
                 {
                     if (q.Name=="Sergei")
                      {
@@ -50,26 +65,6 @@ namespace EFGetStarted
 //     Вывод после изменения
                 Console.WriteLine($"{q.Id}.Имя - {q.Name}, Возраст - {q.Age}, пол - {q.Gender}, роль - {q.Discriminator}");
                 }
-//     Удаление
-               /* Console.WriteLine("Список объектов после удаления Шекша:");
-                foreach (Person q in users2)
-                {
-                    if (q.Name=="Sergei")
-                     {
-                         q.Gender="Pidoras";
-                         db.Persons.Remove(q);
-                         db.SaveChanges();
-                        // break;
-                     }
-//     Вывод после изменения
-                }
-                var users3 = db.Persons.ToList();
-                foreach (Person q in users3)
-                {
-                    Console.WriteLine($"{q.Id}.Имя - {q.Name}, Возраст - {q.Age}, пол - {q.Gender}, роль - {q.Discriminator}");
-                }*/
-
-
                 Lesson lesson1 = new Lesson {Title = "ПЯВУ", Level = 2};
                 Lesson lesson2 = new Lesson {Title = "СЭВМ", Level = 3};
                 //Person person3 = new Person {Name = "Sergei", Surname = "Shekshuev", Age = "28", Gender = "Female"};
@@ -79,30 +74,34 @@ namespace EFGetStarted
 
                 student1.StudentLessons.Add(new StudentLesson { LessonId = lesson1.Id, StudentId = student1.Id });
                 student2.StudentLessons.Add(new StudentLesson { LessonId = lesson2.Id, StudentId = student2.Id });
+                student3.StudentLessons.Add(new StudentLesson { LessonId = lesson1.Id, StudentId = student3.Id });
+                student4.StudentLessons.Add(new StudentLesson { LessonId = lesson2.Id, StudentId = student4.Id });
                 teacher1.TeacherLessons.Add(new TeacherLesson { LessonId = lesson1.Id, TeacherId = teacher1.Id });
                 teacher2.TeacherLessons.Add(new TeacherLesson { LessonId = lesson2.Id, TeacherId = teacher2.Id });
+                
                 db.SaveChanges();
 
+
                  var var_Lessons = db.Lessons.ToList();
-                 var var_Teacher = db.Persons.Select(t => t.Discriminator=="Teacher").ToList();
                  // выводим все курсы
+                 Console.WriteLine();
                  foreach (var c in  var_Lessons)
                     {
-                      Console.WriteLine($"\n Course: {c.Title}");
-                // выводим всех студентов для данного кура
-                      var id_t = (from Id_t in db.TeacherLessons
-                              where Id_t.LessonId == c.Id
-                              select Id_t);
-                      /*Console.WriteLine($"\n Course: {(from Id_t in db.TeacherLessons
-                              where Id_t.LessonId == c.Id
-                              select Id_t)}");*/
+                      Console.WriteLine($" Lessons: {c.Title}");
+                      var id_teacher = (from Id_t in db.TeacherLessons where Id_t.LessonId == c.Id select Id_t.TeacherId).First();
+                // выводим преподов данного курса
+                      Console.WriteLine($" Teacher: {(from t in db.Persons where t.Id == id_teacher select t.Name).First()}");
                       var students = c.StudentLessons.Select(sc => sc.Student).ToList();
-                      foreach (Person s in students)
-                    Console.WriteLine($"{s.Name}");
+
+
+                 // выводим всех студентов для данного кура
+                      Console.Write($" Student:");
+                      foreach (Person s in students) Console.Write($" {s.Name} ");
+                      Console.WriteLine("\n");
                      }
 
             
-                /*  User user = db.Users.FirstOrDefault();*/
+
 
                }
             Console.Read();
